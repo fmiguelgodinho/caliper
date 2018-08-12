5944a7a33534a355dce2993e481de0b13c37661d
Francisco Godinho <f.miguelgodinho@gmail.com>
2018-08-12 17:51:07 +0100

    testing

---
+++ benchmark/ovh-hlf/queryAll.js
@@ -0,0 +1,37 @@
+/**
+* Copyright 2017 HUAWEI. All Rights Reserved.
+*
+* SPDX-License-Identifier: Apache-2.0
+*
+*/
+
+'use strict';
+
+module.exports.info  = 'querying contract specification';
+
+
+let bc, contx;
+
+module.exports.init = function(blockchain, context, args) {
+    bc       = blockchain;
+    contx    = context;
+    return Promise.resolve();
+};
+
+module.exports.run = function() {
+    return bc.invokeSmartContract(contx, 'xcc', 'v1', {verb : 'queryAll'}, 120)
+      .then((results)=>{
+          for (let i in results) {
+              let stat = results[i];
+              if(stat.IsCommitted()) {
+                  console.log(stat.GetResult().toString());
+              }
+          }
+          return Promise.resolve(results);
+    });
+};
+
+module.exports.end = function() {
+    // do nothing
+    return Promise.resolve();
+};
